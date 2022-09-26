a5469e182a45733d16ff2f73d9b636648507aa01
Ebuunejs <fadil.ibrahimi80@gmail.com>
2022-09-26 08:07:11 +0200

    V1.0

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+var combinationSpeechObject = {
+  f1:['f1','i want','avalon','step one','buffalo','revlon','of war','at 1','1:30'],
+  f2:['f2','a poll','app to','have to','i have to','to'],
+  f3:['f3','a free','ivory','every','3'],
+  f4:['f4','a full','a fall','app for','how far','fo'],
+  f5:['f5','i`ve','fbi','afar','at 5'],
+  f6:['f6','epic','abscess','6:30'],
+  f7:['f7','ff7','of 7','57','7:30','7'],
+  f8:['f8','update','8:30'],
+  f9:['f9','9:30','if not','59','9']
+}
+
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = true;
+recognition.maxAlternatives = 1;
+
+recognition.start();
+
+recognition.onresult = function(event) {
+  const field = event.results[0][0].transcript.toLowerCase();
+  console.log('---------------------------')
+  console.log('field: ',field);
+  console.log('---------------------------')
+  for(const key of Object.keys(combinationSpeechObject)){
+      if(combinationSpeechObject[key].indexOf(field) != -1){
+        setField(key); 
+        break;
+      }
+      console.log('Error: key not found.')
+  }
+}
+
+recognition.onend= function(event){
+ /* setTimeout(function(){
+    recognition.start();
+  }, 300);*/
+  recognition.start();
+  console.log("Ende")
+}
+
+recognition.onspeechend = function() {
+  recognition.stop();
+}
+
+recognition.onnomatch = function(event) {
+  console.log("not found",event);
+  //diagnostic.textContent = "I didn't recognise that color.";
+}
+
+recognition.onerror = function(event) {
+  console.log("Error",event.error);
+  //diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
---
+++ assets/js/tic.js
@@ -0,0 +1,67 @@
+var flag = 1;
+var fieldsObject = {}
+var combinationObject = {
+    // horizotanl
+    comb1:['f1','f2','f3'],
+    comb2:['f4','f5','f6'],
+    comb3:['f7','f8','f9'],
+    // vertikal
+    comb4:['f1','f4','f7'],
+    comb5:['f2','f5','f8'],
+    comb6:['f3','f6','f9'],
+    // diagonal
+    comb7:['f1','f5','f9'],
+    comb8:['f3','f5','f7']
+}
+
+function checkWinner(){
+    setup();
+    ['x', 'o'].forEach(flagValue => {
+        for(const comb of Object.values(combinationObject)){
+            for(let i=0;i<3;i++){
+                if(fieldsObject[comb[i]] == '' || fieldsObject[comb[i]] != flagValue) break;
+                if(i >= 2){
+                    document.getElementById('print').innerHTML = `Player  '${flagValue}' wins!`;
+                    disableEmptyFields();
+                }
+            }
+        }
+    })
+}
+
+function disableEmptyFields(){
+    for(const [key, value] of Object.entries(fieldsObject)){
+        if(value == ''){
+            document.getElementById(key).disabled = true;
+        }
+    }
+}
+
+function setup(){
+    for(let i =1;i<=9;i++){
+        fieldsObject['f'+i]=document.getElementById('f'+i).value;
+    }
+}
+ 
+function resetGame() {
+    location.reload();
+    for(let i =0;i<9;i++){
+        document.getElementById('f'+i).value = '';
+    }
+}
+
+function setField(field){
+    if(document.getElementById(field).value === ""){
+        if (flag == 1) {
+            document.getElementById(field).value = "x";
+            document.getElementById(field).disabled = true;
+            flag = 0;
+        }
+        else {
+            document.getElementById(field).value = "o";
+            document.getElementById(field).disabled = true;
+            flag = 1;
+        }
+        checkWinner();
+    }
+}
